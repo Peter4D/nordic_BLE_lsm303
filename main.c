4239b66b6ce097e2987082f251192b3ab2a2bb2e
medve_pe <peter.medvesek@gorenje.com>
2021-02-21 21:47:50 +0100

    - Use of app_timer: Work OK - Move all code regarding the reading of i2c(twi) sensor to separate file (WIP)

--- main.c
+++ main.c
@@ -69,6 +69,11 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
+#include "app_timer.h"
+
+#include "nrf_drv_clock.h"
+#include "nrfx_clock.h"
+
 /* TWI instance ID. */
 #define TWI_INSTANCE_ID     0
 
@@ -85,47 +90,46 @@
 /* Mode for LM75B. */
 #define NORMAL_MODE 0U
 
-typedef enum {                               // DEFAULT    TYPE
-    LSM303_REGISTER_ACCEL_WHO_AM_I = 0x0F,     // 00000111   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG1_A = 0x20,  // 00000111   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG2_A = 0x21,  // 00000000   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG3_A = 0x22,  // 00000000   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG4_A = 0x23,  // 00000000   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG5_A = 0x24,  // 00000000   rw
-    LSM303_REGISTER_ACCEL_CTRL_REG6_A = 0x25,  // 00000000   rw
-    LSM303_REGISTER_ACCEL_REFERENCE_A = 0x26,  // 00000000   r
-    LSM303_REGISTER_ACCEL_STATUS_REG_A = 0x27, // 00000000   r
-    LSM303_REGISTER_ACCEL_OUT_X_L_A = 0x28,
-    LSM303_REGISTER_ACCEL_OUT_X_H_A = 0x29,
-    LSM303_REGISTER_ACCEL_OUT_Y_L_A = 0x2A,
-    LSM303_REGISTER_ACCEL_OUT_Y_H_A = 0x2B,
-    LSM303_REGISTER_ACCEL_OUT_Z_L_A = 0x2C,
-    LSM303_REGISTER_ACCEL_OUT_Z_H_A = 0x2D,
-    LSM303_REGISTER_ACCEL_FIFO_CTRL_REG_A = 0x2E,
-    LSM303_REGISTER_ACCEL_FIFO_SRC_REG_A = 0x2F,
-    LSM303_REGISTER_ACCEL_INT1_CFG_A = 0x30,
-    LSM303_REGISTER_ACCEL_INT1_SOURCE_A = 0x31,
-    LSM303_REGISTER_ACCEL_INT1_THS_A = 0x32,
-    LSM303_REGISTER_ACCEL_INT1_DURATION_A = 0x33,
-    LSM303_REGISTER_ACCEL_INT2_CFG_A = 0x34,
-    LSM303_REGISTER_ACCEL_INT2_SOURCE_A = 0x35,
-    LSM303_REGISTER_ACCEL_INT2_THS_A = 0x36,
-    LSM303_REGISTER_ACCEL_INT2_DURATION_A = 0x37,
-    LSM303_REGISTER_ACCEL_CLICK_CFG_A = 0x38,
-    LSM303_REGISTER_ACCEL_CLICK_SRC_A = 0x39,
-    LSM303_REGISTER_ACCEL_CLICK_THS_A = 0x3A,
-    LSM303_REGISTER_ACCEL_TIME_LIMIT_A = 0x3B,
-    LSM303_REGISTER_ACCEL_TIME_LATENCY_A = 0x3C,
-    LSM303_REGISTER_ACCEL_TIME_WINDOW_A = 0x3D
-}lsm303_Accel_Reg_t;
+// typedef enum {                               // DEFAULT    TYPE
+//     LSM303_REGISTER_ACCEL_WHO_AM_I = 0x0F,     // 00000111   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG1_A = 0x20,  // 00000111   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG2_A = 0x21,  // 00000000   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG3_A = 0x22,  // 00000000   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG4_A = 0x23,  // 00000000   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG5_A = 0x24,  // 00000000   rw
+//     LSM303_REGISTER_ACCEL_CTRL_REG6_A = 0x25,  // 00000000   rw
+//     LSM303_REGISTER_ACCEL_REFERENCE_A = 0x26,  // 00000000   r
+//     LSM303_REGISTER_ACCEL_STATUS_REG_A = 0x27, // 00000000   r
+//     LSM303_REGISTER_ACCEL_OUT_X_L_A = 0x28,
+//     LSM303_REGISTER_ACCEL_OUT_X_H_A = 0x29,
+//     LSM303_REGISTER_ACCEL_OUT_Y_L_A = 0x2A,
+//     LSM303_REGISTER_ACCEL_OUT_Y_H_A = 0x2B,
+//     LSM303_REGISTER_ACCEL_OUT_Z_L_A = 0x2C,
+//     LSM303_REGISTER_ACCEL_OUT_Z_H_A = 0x2D,
+//     LSM303_REGISTER_ACCEL_FIFO_CTRL_REG_A = 0x2E,
+//     LSM303_REGISTER_ACCEL_FIFO_SRC_REG_A = 0x2F,
+//     LSM303_REGISTER_ACCEL_INT1_CFG_A = 0x30,
+//     LSM303_REGISTER_ACCEL_INT1_SOURCE_A = 0x31,
+//     LSM303_REGISTER_ACCEL_INT1_THS_A = 0x32,
+//     LSM303_REGISTER_ACCEL_INT1_DURATION_A = 0x33,
+//     LSM303_REGISTER_ACCEL_INT2_CFG_A = 0x34,
+//     LSM303_REGISTER_ACCEL_INT2_SOURCE_A = 0x35,
+//     LSM303_REGISTER_ACCEL_INT2_THS_A = 0x36,
+//     LSM303_REGISTER_ACCEL_INT2_DURATION_A = 0x37,
+//     LSM303_REGISTER_ACCEL_CLICK_CFG_A = 0x38,
+//     LSM303_REGISTER_ACCEL_CLICK_SRC_A = 0x39,
+//     LSM303_REGISTER_ACCEL_CLICK_THS_A = 0x3A,
+//     LSM303_REGISTER_ACCEL_TIME_LIMIT_A = 0x3B,
+//     LSM303_REGISTER_ACCEL_TIME_LATENCY_A = 0x3C,
+//     LSM303_REGISTER_ACCEL_TIME_WINDOW_A = 0x3D
+// }lsm303_Accel_Reg_t;
 
 /* Indicates if operation on TWI has ended. */
-static volatile bool m_xfer_done = true;
+//static volatile bool m_xfer_done = true;
 
-//#define NRFX_TWI0_ENABLED 1
-/* TWI instance. */
-//static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
-static const nrfx_twi_t m_twi = NRFX_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+// /* TWI instance. */
+// static const nrfx_twi_t m_twi = NRFX_TWI_INSTANCE(TWI_INSTANCE_ID);
 
 // #if(NRFX_TWI_ENABLED_COUNT > 0)
 // static const nrfx_twi_t m_twi = {
@@ -139,6 +143,10 @@ static const nrfx_twi_t m_twi = NRFX_TWI_INSTANCE(TWI_INSTANCE_ID);
 static uint8_t m_who_i_am;
 
 static uint8_t m_btn_state = 0;
+
+APP_TIMER_DEF(app_tmr1_id);
+
+void app_tmr1_id_handler(void* p_context);
 
 
 
@@ -158,7 +166,6 @@ static uint8_t m_btn_state = 0;
 
 void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-    //nrf_drv_gpiote_out_toggle(PIN_OUT);
     nrfx_gpiote_out_toggle(PIN_OUT);
 }
 /**
@@ -169,49 +176,41 @@ static void gpio_init(void)
 {
     ret_code_t err_code;
 
-    err_code = nrf_drv_gpiote_init();
+    err_code = nrfx_gpiote_init();
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_out_config_t out_config = GPIOTE_CONFIG_OUT_SIMPLE(false);
+    nrfx_gpiote_out_config_t out_config = GPIOTE_CONFIG_OUT_SIMPLE(false);
 
-    err_code = nrf_drv_gpiote_out_init(PIN_OUT, &out_config);
+    err_code = nrfx_gpiote_out_init(PIN_OUT, &out_config);
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_TOGGLE(true);
+    //nrfx_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_TOGGLE(true);
+    nrfx_gpiote_in_config_t in_config = NRFX_GPIOTE_RAW_CONFIG_IN_SENSE_HITOLO(true);
     in_config.pull = NRF_GPIO_PIN_PULLUP;
 
-    err_code = nrf_drv_gpiote_in_init(PIN_IN, &in_config, in_pin_handler);
+    err_code = nrfx_gpiote_in_init(PIN_IN, &in_config, in_pin_handler);
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_in_event_enable(PIN_IN, true);
+    nrfx_gpiote_in_event_enable(PIN_IN, true);
 }
 
 
 
-/**
- * @brief Function for setting active mode on MMA7660 accelerometer.
- */
-void lsm303_setup(void)
-{
-    ret_code_t err_code;
+// /**
+//  * @brief Function for setting active mode on MMA7660 accelerometer.
+//  */
+// void lsm303_setup(void)
+// {
+//     ret_code_t err_code;
 
-    /* Writing to LM75B_REG_CONF "0" set temperature sensor in NORMAL mode. */
-    //uint8_t reg[2] = {LM75B_REG_CONF, NORMAL_MODE};
+//     /* setup lsm303 accel default */
+//     uint8_t reg[2] = {LSM303_REGISTER_ACCEL_CTRL_REG1_A, 0x57};
 
-    /* setup lsm303 accel default */
-    uint8_t reg[2] = {LSM303_REGISTER_ACCEL_CTRL_REG1_A, 0x57};
+//     err_code = nrfx_twi_tx(&m_twi, LSM303_ACCEL_ADDR, reg, sizeof(reg), false);
+//     APP_ERROR_CHECK(err_code);
+//     while (m_xfer_done == false);
 
-    err_code = nrfx_twi_tx(&m_twi, LSM303_ACCEL_ADDR, reg, sizeof(reg), false);
-    APP_ERROR_CHECK(err_code);
-    while (m_xfer_done == false);
-
-    /* Writing to pointer byte. */
-    // reg[0] = LM75B_REG_TEMP;
-    // m_xfer_done = false;
-    // err_code = nrf_drv_twi_tx(&m_twi, LSM303_ACCEL_ADDR, reg, 1, false);
-    // APP_ERROR_CHECK(err_code);
-    // while (m_xfer_done == false);
-}
+// }
 
 /**
  * @brief Function for handling data from temperature sensor.
@@ -226,65 +225,95 @@ __STATIC_INLINE void data_handler(uint8_t temp)
 /**
  * @brief TWI events handler.
  */
-void twi_handler(nrfx_twi_evt_t const * p_event, void * p_context)
-{
-    switch (p_event->type)
-    {
-        case NRFX_TWI_EVT_DONE:
-            if (p_event->xfer_desc.type == NRFX_TWI_XFER_RX)
-            {
-                data_handler(m_who_i_am);
-            }
-            m_xfer_done = true;
-            break;
-        default:
-            break;
-    }
-}
+// void twi_handler(nrfx_twi_evt_t const * p_event, void * p_context)
+// {
+//     switch (p_event->type)
+//     {
+//         case NRFX_TWI_EVT_DONE:
+//             if (p_event->xfer_desc.type == NRFX_TWI_XFER_RX)
+//             {
+//                 data_handler(m_who_i_am);
+//             }
+//             m_xfer_done = true;
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
-/**
- * @brief UART initialization.
- */
-void twi_init (void)
-{
-    ret_code_t err_code;
 
-    const nrfx_twi_config_t twi_lsm303_config = {
-       .scl                = ARDUINO_SCL_PIN,
-       .sda                = ARDUINO_SDA_PIN,
-       .frequency          = NRF_TWI_FREQ_100K,
-       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
-       .hold_bus_uninit    = false
-    };
+// void twi_init (void)
+// {
+//     ret_code_t err_code;
 
-    err_code = nrfx_twi_init(&m_twi, &twi_lsm303_config, twi_handler, NULL);
-    APP_ERROR_CHECK(err_code);
+//     const nrfx_twi_config_t twi_lsm303_config = {
+//        .scl                = ARDUINO_SCL_PIN,
+//        .sda                = ARDUINO_SDA_PIN,
+//        .frequency          = NRF_TWI_FREQ_100K,
+//        .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+//        .hold_bus_uninit    = false
+//     };
 
-    nrfx_twi_enable(&m_twi);
-}
+//     err_code = nrfx_twi_init(&m_twi, &twi_lsm303_config, twi_handler, NULL);
+//     APP_ERROR_CHECK(err_code);
+
+//     nrfx_twi_enable(&m_twi);
+// }
 
 /**
  * @brief Function for reading data from temperature sensor.
  */
-static void read_sensor_data()
-{
-    uint8_t reg[1] = {LSM303_REGISTER_ACCEL_WHO_AM_I};
-    volatile ret_code_t err_code = 0xFF;
-    m_xfer_done = false;
+// static void read_sensor_data()
+// {
+//     uint8_t reg[1] = {LSM303_REGISTER_ACCEL_WHO_AM_I};
+//     volatile ret_code_t err_code = 0xFF;
+//     m_xfer_done = false;
 
+//     err_code = nrfx_twi_tx(&m_twi, LSM303_ACCEL_ADDR, reg, sizeof(reg), true);
+//     APP_ERROR_CHECK(err_code);
+//     while(m_xfer_done == false);
     
-    err_code = nrfx_twi_tx(&m_twi, LSM303_ACCEL_ADDR, reg, sizeof(reg), true);
-    APP_ERROR_CHECK(err_code);
-    while(m_xfer_done == false);
-    
-    err_code = nrfx_twi_rx(&m_twi, LSM303_ACCEL_ADDR, &m_who_i_am, sizeof(m_who_i_am));
-    APP_ERROR_CHECK(err_code);
-}
+//     err_code = nrfx_twi_rx(&m_twi, LSM303_ACCEL_ADDR, &m_who_i_am, sizeof(m_who_i_am));
+//     APP_ERROR_CHECK(err_code);
+// }
 
 // void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 // {
 //     m_btn_state = 1;
 // }
+
+void app_tmr1_id_handler(void* p_context) {
+    static uint32_t heart_beat = 0;
+
+    NRF_LOG_INFO("app_timer: %d", heart_beat++);
+    //bsp_board_led_invert(LED_RED);  
+    bsp_board_led_invert(bsp_board_pin_to_led_idx(LED_RED));
+
+    NRF_LOG_FLUSH();
+}
+
+
+void clock_event_handler(nrfx_clock_evt_type_t event)
+{
+}
+
+/**@brief Function starting the internal LFCLK oscillator.
+ *
+ * @details This is needed by RTC1 which is used by the Application Timer
+ *          (When SoftDevice is enabled the LFCLK is always running and this is not needed).
+ */
+static void lfclk_request(void)
+{
+    ret_code_t err_code;
+    //nrfx_clock_init(clock_event_handler);
+    // APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_clock_lfclk_request(NULL);
+    //nrfx_clock_lfclk_start();
+}
 
 /**
  * @brief Function for main application entry.
@@ -305,8 +334,10 @@ int main(void)
     /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS);
 
-    twi_init();
-    lsm303_setup();
+    //twi_init();
+    //lsm303_setup();
+    lfclk_request();
+    app_timer_init();
     gpio_init();
 
     // nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_TOGGLE(true);
@@ -322,17 +353,23 @@ int main(void)
 
     btn_idx = bsp_board_pin_to_button_idx(BUTTON_1);
     //btn_2_idx = bsp_board_pin_to_button_idx(BUTTON_2);
-		
+
+    app_timer_create(&app_tmr1_id,
+                        APP_TIMER_MODE_REPEATED,
+                        app_tmr1_id_handler);
+    
+    volatile uint32_t periode = APP_TIMER_TICKS(1000);
+    app_timer_start(app_tmr1_id, periode, NULL);
 
     while (true)
     {
-        static uint32_t heart_beat = 0;
-        volatile uint8_t btn_state = 0xFF; 
-				//uint32_t led_idx = bsp_board_pin_to_led_idx(LED_RED);
-				uint32_t led_idx = bsp_board_pin_to_led_idx(LED_1);
+        // static uint32_t heart_beat = 0;
+        // volatile uint8_t btn_state = 0xFF; 
+        //uint32_t led_idx = bsp_board_pin_to_led_idx(LED_RED);
+        //uint32_t led_idx = bsp_board_pin_to_led_idx(LED_1);
 
-        nrf_delay_ms(50);
-        NRF_LOG_INFO("i am alive: %d", heart_beat++);
+        //nrf_delay_ms(50);
+        //NRF_LOG_INFO("i am alive: %d", heart_beat++);
         
         
 
@@ -366,13 +403,13 @@ int main(void)
         //     NRF_LOG_FLUSH();
         // }
 
-        do
-        {
-            __WFE();
-        }while (m_xfer_done == false);
+        // do
+        // {
+        //     __WFE();
+        // }while (m_xfer_done == false);
 
-        read_sensor_data();
-        NRF_LOG_FLUSH();
+        //read_sensor_data();
+        //NRF_LOG_FLUSH();
     }
 }
 
